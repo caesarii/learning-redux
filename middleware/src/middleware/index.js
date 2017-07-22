9e61b69fe57f5898b627c28f8b8037c84901476b
qinghe <qinghe@2dfire.com>
2017-07-22 17:42:22 +0800

    middleware

--- middleware/src/middleware/index.js
+++ middleware/src/middleware/index.js
@@ -1,12 +1,11 @@
-
 const logger = store => next => action => {
-  console.group(action.type)
-  console.info('dispatching', action)
-  console.log('next', next)
-  let result = next(action)
-  console.log('next state', store.getState())
-  console.groupEnd(action.type)
-  return result
+    console.group(action.type)
+    console.info('dispatching', action)
+    console.log('next', next)
+    let result = next(action)
+    console.log('next state', store.getState())
+    console.groupEnd(action.type)
+    return result
 }
 
 // const logger = store => {
@@ -26,36 +25,36 @@ const logger = store => next => action => {
 
 
 const crashReporter = store => next => action => {
-  try{
-    console.log('crashReporter')
-    console.log('next', next)
-    return next(action)
-  } catch (err) {
-    console.error('Caught an exceptional', err)
-    throw err
-  }
+    try {
+        console.log('crashReporter')
+        console.log('next', next)
+        return next(action)
+    } catch (err) {
+        console.error('Caught an exceptional', err)
+        throw err
+    }
 }
 
 const middlewareOne = store => next => action => {
-  console.log('middlewareOne')
-  console.log('next', next)
-  return next(action)
+    console.log('middlewareOne')
+    console.log('next', next)
+    return next(action)
 }
 
 const middlewareTwo = store => next => action => {
-  console.log('middlewareTwo')
-  console.log('next', next)
-  return next(action)
+    console.log('middlewareTwo')
+    console.log('next', next)
+    return next(action)
 }
 
 const middlewareThree = store => next => action => {
-  console.log('middlewareThree')
-  console.log('next', next)
-  return next(action)
+    console.log('middlewareThree')
+    console.log('next', next)
+    return next(action)
 }
 
 
-export {logger, crashReporter, middlewareOne, middlewareTwo, middlewareThree}
+export { logger, crashReporter, middlewareOne, middlewareTwo, middlewareThree }
 
 
 
